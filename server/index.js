7e71656a01ac2ac347ff072372b5475bfa068981
sheeeuWu <shefali22119@gmail.com>
2024-06-04 01:19:28 +0530

    removed helmet.corPolicy

--- server/index.js
+++ server/index.js
@@ -25,7 +25,7 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(helmet());
-app.use(helmet.crossOriginResourcePolicy({policy: "cross-origin"}));
+// app.use(helmet.crossOriginResourcePolicy({policy: "cross-origin"}));
 app.use(morgan("common"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended : false}));
